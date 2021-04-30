eb64e709d9e567d9b0723800fe9d6f027b451f27
RicardoFederal <rmviaro@gmail.com>
2021-04-30 19:52:46 -0300

    inserindo javascritp botão mais e menos

--- script.js
+++ script.js
@@ -44,3 +44,39 @@ pizzaJson.map((item, index)=>{
    
     c('.pizza-area').append(pizzaItem); //anexa o item a item (apend)
 });
+
+// eventos do modal
+
+function fecharModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+
+    },500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', fecharModal);
+});
+
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+   
+    if(quant >1 ){
+   quant--;
+   c('.pizzaInfo--qt').innerHTML = quant;
+    }
+});
+
+   
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+   quant++;
+   c('.pizzaInfo--qt').innerHTML = quant; 
+});
+
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
